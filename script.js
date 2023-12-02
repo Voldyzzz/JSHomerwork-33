04023a7bd264f44e816a8f6ebbe52dae17aee326
Voldyzzz <volodymyr.liuliev2000@gmail.com>
2023-12-02 23:17:17 +0200

    update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const ref = {
   infoBlocs: document.querySelector(".info-block").children,
 };
 
-// const productsBlocksArray = Array(ref.productsBlocks);
+const productsBlocksArray = Array(ref.productsBlocks);
 const infoBlocsArray = Array(ref.infoBlocs);
 
 ref.body.addEventListener("click", (event) => {
@@ -16,23 +16,53 @@ function handlerOpenProductsClick(event) {
   const target = event.target;
   if (target.nodeName === "LI") {
     ref.categories.forEach((category, index) => {
-      ref.productsBlocks[index].classList.add("hidden");
+      hiddenAllElement(index);
       if (category === target) {
         ref.productsBlocks[index].classList.remove("hidden");
         ref.productsBlocks[index].classList.add("visible");
 
         ref.productsBlocks[index].addEventListener("click", (event) => {
-          if (
-            (event.target.nodeName === "DIV" &&
-              event.target.classList.contains("card")) ||
-            event.target.nodeName === "IMG" ||
-            event.target.nodeName === "P" ||
-            event.target.nodeName === "H3"
-          ) {
-            const arr = infoBlocsArray[0][index].children;
-          }
+          handlerOpenInfoBlockClick(event, index);
         });
       }
     });
   }
+}
+
+function handlerOpenInfoBlockClick(event, index) {
+  const target = event.target;
+  if (
+    (target.nodeName === "DIV" && target.classList.contains("card")) ||
+    target.nodeName === "IMG" ||
+    target.nodeName === "P" ||
+    target.nodeName === "H3"
+  ) {
+    openInfoBlock(index, event);
+  }
+}
+
+function openInfoBlock(index, event) {
+  const arrProduct = productsBlocksArray[0][index].children;
+  const arrInfo = infoBlocsArray[0][index].children;
+  let arrProductWithChilds;
+  for (let i = 0; i <= arrProduct.length - 1; i++) {
+    arrProductWithChilds = arrProduct[i].children;
+    for (let j = 0; j <= arrProductWithChilds.length - 1; j++) {
+      if (
+        event.target === arrProduct[i] ||
+        event.target === arrProductWithChilds[j]
+      ) {
+        arrInfo[i].classList.remove("hidden");
+        arrInfo[i].classList.add("visible");
+      }
+    }
+  }
+}
+
+function hiddenAllElement(index) {
+  ref.productsBlocks[index].classList.add("hidden");
+
+  const arrWithChildsElements = infoBlocsArray[0][index].children;
+  for (let i = 0; i <= arrWithChildsElements.length - 1; i++)
+    arrWithChildsElements[i].classList.add("hidden");
 }
